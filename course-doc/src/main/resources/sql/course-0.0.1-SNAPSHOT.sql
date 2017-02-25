eca5c24a59ef9825a13a3f5abc0a53b6acfebeef
master-alen <nchufujianjian@126.com>
2017-02-25 18:40:18 +0800

    日常代码提交

--- course-doc/src/main/resources/sql/course-0.0.1-SNAPSHOT.sql
+++ course-doc/src/main/resources/sql/course-0.0.1-SNAPSHOT.sql
@@ -1,1 +1,30 @@
 # 课程系统更新sql脚本文件
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50624
+Source Host           : localhost:3306
+Source Database       : course
+
+Target Server Type    : MYSQL
+Target Server Version : 50624
+File Encoding         : 65001
+
+Date: 2017-02-25 18:39:16
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for course_user
+-- ----------------------------
+DROP TABLE IF EXISTS `course_user`;
+CREATE TABLE `course_user` (
+  `id` varchar(32) NOT NULL,
+  `name` varchar(16) DEFAULT NULL,
+  `nickname` varchar(32) DEFAULT NULL,
+  `created_time` datetime DEFAULT NULL,
+  `modified_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
